0cb888caa7ca53c4fa3b09a003c95eed64e4dd62
Silvia Arnaiz Garcia <silviaarnaizgarcia@FVFXM3W0HV29.local>
2020-07-07 11:03:34 +0200

    Add input component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { YtpButtonComponent } from './ytp-button';
+import { YtpFieldComponent } from './ytp-field';
 
-const components = [ YtpButtonComponent ];
+const components = [
+  YtpButtonComponent,
+  YtpFieldComponent
+];
 
 @NgModule({
   declarations: [...components],
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FormsModule ],
   exports: [...components],
 })
 export class ComponentsModule {}
---
+++ src/components/ytp-field/ytp-field.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'ytp-field',
+  templateUrl: './ytp-field.component.html',
+  styleUrls: ['./ytp-field.component.css']
+})
+export class YtpFieldComponent {
+
+  @Input()
+  public label: string
+
+  @Input()
+  public error = '';
+
+  private fieldValue: string;
+
+  @Input()
+  get inputValue() {
+    return this.fieldValue;
+  }
+
+  @Output()
+  public inputValueChange: EventEmitter<string> = new EventEmitter<string>();
+
+  @Output()
+  public onBlur: EventEmitter<string> = new EventEmitter<string>();
+
+  set inputValue(value) {
+    this.fieldValue = value;
+    this.inputValueChange.emit(value);
+  }
+
+  public blurField(event) {
+    this.onBlur.emit(event.target.value);
+  }
+
+
+}
